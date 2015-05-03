31a883351a8cff5a5844c893a3c8ee13a37f55cb
Oleg Blinnikov <osblinnikov@gmail.com>
2015-05-03 12:15:31 +0200

    added milisec wrapper for taskDelay

--- types/timeUtils.h
+++ types/timeUtils.h
@@ -41,6 +41,11 @@ types_cnets_osblinnikov_github_com_EXPORT_API
     int taskDelayTill(struct timespec *end,  void* mutex, void* condition_variable);
 types_cnets_osblinnikov_github_com_EXPORT_API
     void taskDelay(uint64_t nanosec);
+
+inline void taskDelayMilisec(uint64_t milisec){
+    taskDelay(milisec*1000L*1000L);
+}
+
 types_cnets_osblinnikov_github_com_EXPORT_API
     unsigned diffTwoTimespecs(struct timespec *lTimeSpec,struct timespec *rTimeSpec);
 #ifdef __cplusplus
