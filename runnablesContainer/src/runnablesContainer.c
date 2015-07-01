8577b53c5302deee7c8d71dd2d7ffb94293c54d2
Oleg Blinnikov <osblinnikov@gmail.com>
2015-07-01 22:54:04 +0200

    onStart and onStop launched only after all kernels are finished

--- runnablesContainer/src/runnablesContainer.c
+++ runnablesContainer/src/runnablesContainer.c
@@ -25,52 +25,67 @@ void RunnableStoppable_init(struct RunnableStoppable* that){
   that->target = NULL;
 }
 
-void runnablesContainer_cnets_osblinnikov_github_com_launch(
-  struct runnablesContainer_cnets_osblinnikov_github_com *that, 
-  BOOL lockLastElement
+void runnablesContainer_cnets_osblinnikov_github_com_doLaunch(
+  struct runnablesContainer_cnets_osblinnikov_github_com *that,
+  BOOL lockLastElement, BOOL isOnStart
 ){
   if(that->containers != NULL && that->containers_size > 0){
     int i;
     for(i=0; i< that->containers_size; i++){
-      if(that->target.target != NULL){
+      if(isOnStart && that->target.target != NULL){
         printf("=> launch: %s mode=%d\n",that->target.name,that->spawnMode);
         that->target.onStart(that->target.target);
       }
-      that->containers[i].launch(&that->containers[i], (lockLastElement && i == that->containers_size - 1) );
+      runnablesContainer_cnets_osblinnikov_github_com_doLaunch(&that->containers[i], (lockLastElement && i == that->containers_size - 1), isOnStart );
     }
   }else if(that->target.target != NULL){
     printf("=> launch: %s mode=%d\n",that->target.name,that->spawnMode);
-    if(that->spawnMode == 1){
+    if(that->spawnMode == 1 && !isOnStart){
       that->kernel.launch(&that->kernel, that->target, lockLastElement);
-    }else{
+    }else if(isOnStart){
       that->target.onStart(that->target.target);
     }
-
   }else {
     printf("===> runnablesContainer_cnets_osblinnikov_github_com_launch NULL\n");
+  }
+}
+
+void runnablesContainer_cnets_osblinnikov_github_com_launch(
+  struct runnablesContainer_cnets_osblinnikov_github_com *that,
+  BOOL lockLastElement
+){
+  runnablesContainer_cnets_osblinnikov_github_com_doLaunch(that,lockLastElement, TRUE);
+  runnablesContainer_cnets_osblinnikov_github_com_doLaunch(that,lockLastElement, FALSE);
+}
+
+void runnablesContainer_cnets_osblinnikov_github_com_doStop(
+  struct runnablesContainer_cnets_osblinnikov_github_com *that,
+  BOOL isOnStop
+){
+  if(that->containers != NULL && that->containers_size > 0){
+    int i;
+    for(i=that->containers_size -1; i>=0; i--){
+      runnablesContainer_cnets_osblinnikov_github_com_doStop(&that->containers[i], isOnStop);
+      if(isOnStop && that->target.target != NULL){
+        that->target.onStop(that->target.target);
+      }
+    }
+  }else if(that->target.target != NULL){
+    if(that->spawnMode == 1 && !isOnStop){
+      that->kernel.stopThread(&that->kernel);
+    }else if(isOnStop){
+      that->target.onStop(&that->target.target);
+    }
+  }else{
+    printf("===> runnablesContainer_cnets_osblinnikov_github_com_stop NULL\n");
   }
 }
 
 void runnablesContainer_cnets_osblinnikov_github_com_stop(
   struct runnablesContainer_cnets_osblinnikov_github_com *that
 ){
-  if(that->containers != NULL && that->containers_size > 0){
-    int i;
-    for(i=that->containers_size -1; i>=0; i--){
-      that->containers[i].stop(&that->containers[i]);
-      if(that->target.target != NULL){
-        that->target.onStop(that->target.target);
-      }
-    }
-  }else if(that->target.target != NULL){
-    if(that->spawnMode == 1){
-      that->kernel.stopThread(&that->kernel);
-    }else{
-      that->target.onStop(&that->target.target);
-    }
-  }else{
-    printf("===> runnablesContainer_cnets_osblinnikov_github_com_stop NULL\n");
-  }
+  runnablesContainer_cnets_osblinnikov_github_com_doStop(that, FALSE);
+  runnablesContainer_cnets_osblinnikov_github_com_doStop(that, TRUE);
 }
 
 void runnablesContainer_cnets_osblinnikov_github_com_setContainers(
